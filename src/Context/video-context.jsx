78d78f690f20f80bf5e26717dfb9b26c32e4bcfe
aryan-13 <aryanchaudhary1381@gmail.com>
2022-06-26 23:21:40 +0530

    added init state in context

--- src/Context/video-context.jsx
+++ src/Context/video-context.jsx
@@ -1,12 +1,21 @@
 import { useContext, createContext, useReducer } from 'react';
 
-const videoInit = {};
+const videoInit = {
+	playlists: [],
+	liked: [],
+	watchlater: [],
+	history: [],
+};
 const videoReducer = (state, action) => {};
 const videoContext = createContext();
 
 const VideoProvider = ({ children }) => {
 	const [videoState, videoDispatch] = useReducer(videoReducer, videoInit);
-	return <videoContext.Provider value={{}}>{children}</videoContext.Provider>;
+	return (
+		<videoContext.Provider value={{ videoState, videoDispatch }}>
+			{children}
+		</videoContext.Provider>
+	);
 };
 const useVideo = () => useContext(videoContext);
 export { useVideo, VideoProvider };
