ffef206580bdaa6f67aefebb10202931c091c1fa
aryan-13 <aryanchaudhary1381@gmail.com>
2022-07-13 18:39:54 +0530

    Added context file

--- src/Components/Playlists/Playlist.jsx
+++ src/Components/Playlists/Playlist.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import './Playlist.css';
+import PlaylistTile from './PlaylistTile';
 function Playlist() {
-	return <div>Playlist</div>;
+	return (
+		<div className="flex-row">
+			<PlaylistTile />
+			<PlaylistTile />
+			<PlaylistTile />
+			<PlaylistTile />
+		</div>
+	);
 }
 
 export default Playlist;
---
+++ src/Components/Playlists/PlaylistTile.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PlaylistTile() {
+	return <div className="tiles">Favourites</div>;
+}
+
+export default PlaylistTile;
